42610c492cb8b76d87f37ee8ba3f233db14eeb4c
sametyaprak <sametyaprak@gmail.com>
2020-06-22 10:07:14 +0200

    third commit local repository

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,8 +5,8 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("first commit from local repository");
-		System.out.println("second commmit from locak repository");
-		
+		System.out.println("second commmit from local repository");
+		System.out.println("third commit from local repository");
 	}
 
 }
