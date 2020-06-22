c2deb1c23b91a80d7c2026b8c6effc3cc286c1be
sametyaprak <sametyaprak@gmail.com>
2020-06-22 10:28:04 +0200

    create a confilict

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -6,7 +6,7 @@ public class FirstClass {
 		
 		System.out.println("first commit from local repository");
 		System.out.println("second commmit from local repository");
-		System.out.println("third commit from local repository");
+		System.out.println("3. satirda degisiklik yapiyorum");
 		System.out.println("first commit from remote repository");
 		
 	}
