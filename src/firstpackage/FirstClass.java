64c69c524e50fae8429c091ba4823ee84bb322dd
sametyaprak <sametyaprak@gmail.com>
2020-06-22 10:43:22 +0200

    conflict resolved

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -4,11 +4,11 @@ public class FirstClass {
 	
 	public static void main(String[] args) {
 		
-<<<<<<< HEAD
-		System.out.println("yeni bit karisiklik");
-=======
+
+		System.out.println("yeni bir karisiklik");
+
 		System.out.println("bende degisiklik yaptim");
->>>>>>> b3cbcdbb8615b0045cf24d11b83376a80bc220a1
+
 		System.out.println("second commmit from local repository");
 		System.out.println("3. satirda degisiklik yapiyorum");
 		System.out.println("lets create a conflict");
