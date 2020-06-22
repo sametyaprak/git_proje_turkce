b3cbcdbb8615b0045cf24d11b83376a80bc220a1
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2020-06-22 10:39:34 +0200

    Update FirstClass.java

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -4,7 +4,7 @@ public class FirstClass {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("first commit from local repository");
+		System.out.println("bende degisiklik yaptim");
 		System.out.println("second commmit from local repository");
 		System.out.println("3. satirda degisiklik yapiyorum");
 		System.out.println("lets create a conflict");
